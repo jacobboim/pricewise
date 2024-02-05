eef81d66cf985d224acef0f1ead2cd06284d8710
jacobboim <jacob.boim30@gmail.com>
2024-02-04 20:53:23 -0500

    fix max

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,7 +10,7 @@ import {
 } from "@/lib/actions/utils";
 import { NextResponse } from "next/server";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 9; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
